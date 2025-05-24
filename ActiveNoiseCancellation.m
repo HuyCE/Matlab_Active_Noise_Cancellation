1bc2e0889312cb7f371949f3d06be49122810b85
shiroWorkstation <kagayakashi2k@gmail.com>
2025-05-25 02:42:14 +0700

    init commit

---
+++ ActiveNoiseCancellation.m
@@ -0,0 +1,6 @@
+function [cleanAudio] = ActiveNoiseCancellation(noise_mic,AudioInput)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
